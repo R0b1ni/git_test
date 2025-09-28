55c85bf0e5e454ad204b080cd7d6a4db0466b649
R0b1ni <thononrobin@gmail.com>
2025-09-28 16:50:33 +0200

    Ajout de l'affichage des résultats pour la caractéristique de la machine

--- lab1.py
+++ lab1.py
@@ -30,3 +30,14 @@ B = np.array([5, 2, 7])
 
 solution = np.linalg.solve(A, B)
 print("Solution du système : x = {:.3f}, y = {:.3f}, z = {:.3f}".format(*solution))
+
+# Affichage des résultats
+plt.figure(figsize=(10, 6))
+plt.subplot(2, 1, 1)
+plt.plot(omega, Ua, 'o-', label='Tension (Ua)')
+plt.plot(omega, [Ra*i for i in ia], 's--', label='Chute de tension (Ra*ia)')
+plt.xlabel('Vitesse angulaire (omega)')
+plt.ylabel('Tension (V)')
+plt.title('Caractéristique de la machine')
+plt.legend()
+plt.grid()
